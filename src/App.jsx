221b298b305b75a6447c8699bcba5580eb17e5d2
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-05-31 23:40:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import LeftMenu from 'components/LeftMenu/LeftMenu'
+import './App.css'
+
+function App() {
+	
+
+	return (
+		<>
+			<LeftMenu/>
+		</>
+	)
+}
+
+export default App
---
+++ src/components/LeftMenu/LeftMenu.jsx
@@ -0,0 +1,102 @@
+
+
+
+import HomeIcon from "components/icons/icon-menu/HomeIcon"
+
+
+import "./left-menu.scss";
+import BagIcon from "components/icons/icon-menu/BagIcon";
+import GameIcon from "components/icons/icon-menu/GameIcon";
+import ChartIcon from "components/icons/icon-menu/ChartIcon";
+import ActivityIcon from "components/icons/icon-menu/ActivityIcon";
+import GmaerRulletteIcon from "components/icons/icon-menu/GmaerRulletteIcon";
+import ProfileIcon from "components/icons/icon-menu/ProfileIcon";
+import PetHouseIcon from "components/icons/icon-menu/PetHouseIcon";
+import SettingsIcon from "components/icons/icon-menu/SettingsIcon";
+import LogoutIcon from "components/icons/icon-menu/LogoutIcon";
+import { useEffect, useRef, useState } from "react";
+
+
+const data = [
+    {
+        component: <HomeIcon/>
+    },
+    {
+        component: <BagIcon/>
+    },
+    {
+        component: <GameIcon/>
+    },
+    {
+        component: <ChartIcon/>
+    },
+    {
+        component: <ActivityIcon/>
+    },
+    {
+        component: <GmaerRulletteIcon/>
+    },
+    {
+        component: <ProfileIcon/>
+    },
+    {
+        component: <PetHouseIcon/>
+    },
+    {
+        component: <SettingsIcon/>
+    },
+    {
+        component: <LogoutIcon/>
+    },
+]
+
+
+const LeftMenu = () => {
+
+    const [state, setState] = useState(0);
+
+    const refIndicator = useRef()
+
+
+    const onChangeActivePosition = () => {
+        const element = refIndicator.current;
+        element.style.cssText = `
+            top:${65 * state}px;
+        `
+    }
+
+    useEffect(() => {
+        onChangeActivePosition()
+    }, [state])
+    return (
+        <div className="left-menu">
+            <a href="" className="left-menu__logo">LOGO</a>
+            <nav className="left-menu__navbar">
+                <ul className="left-menu__list">
+                    <li ref={refIndicator} className="indicator">
+                        <div className="menu__indicator-1"></div>
+                        <div className="menu__indicator-2"></div>
+                        <div className="menu__indicator-3"></div>
+                        <div className="menu__indicator-4"></div>
+                        <div className="menu__indicator-5"></div>
+                    </li>
+                    {
+                        data.map((item, i) => {
+                            return (
+                                <li key={i} className="left-menu__item">
+                                    <a onClick={() => setState(i)} href="#" className="left-menu__link">
+                                        {item.component}
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
+                    
+                    
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default LeftMenu
